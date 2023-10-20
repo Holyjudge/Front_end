c1bafb382c9996ba2e4ae4e849b8371701b7edde
Holyjudge <91899012+Holyjudge@users.noreply.github.com>
2023-10-20 11:17:59 +0530

    Create greeting.jsx

---
+++ greeting.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Greeting extends React.Component {
+  render() {
+    const { name } = this.props;
+
+    return (
+      <div>
+        <h1>Hi, {name}!</h1>
+      </div>
+    );
+  }
+}
+
+export default Greeting;
